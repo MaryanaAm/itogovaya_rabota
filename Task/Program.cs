5c5b80d4ac80d7a182d4b994d412521827cde146
MaryanaAm <marydeaddoll@gmail.com>
2023-01-25 22:58:12 +0300

    add task solution

---
+++ Task/Program.cs
@@ -0,0 +1,26 @@
+﻿//Объявление массива;
+string[] array = new string[4] {"hello", "2", "world", ":-)"};
+//Объявление нового массива для записи результатов проверки условия;
+string[] newArray = new string[array.Length];
+
+//Вывод массива в консоль для наглядного сравнения с результатом
+for (int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine(array[i]);
+}
+//Создание цикла и проверка условия
+for (int i = 0; i < array.Length; i++)
+{
+    if(array[i].Length <= 3)
+    {
+        newArray[i] = array[i];
+    }
+}
+ Console.WriteLine();
+
+ Console.WriteLine("Результат: ");
+//Вывод результата в консоль.
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write(newArray[i] + " ");
+}
